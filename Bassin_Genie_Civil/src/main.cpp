0fc1ea4b55874e52efe587780171aa5479601ca6
felixduguay11 <166640724+felixduguay11@users.noreply.github.com>
2026-02-20 11:18:56 -0500

    Add files via upload

---
+++ Bassin_Genie_Civil/src/main.cpp
@@ -0,0 +1,210 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <MadgwickAHRS.h>
+
+const int trigPin = 6;
+const int echoPin = 7;
+
+// ===== PID =====
+float distance_ref = 15;
+float prev_target = 15;
+float error = 0;
+float prevError = 0;
+float integral = 0;
+float derivative = 0;
+
+// Gains (seront modifiés par fuzzy)
+float Kp = 1.0;
+float Ki = 0.0;
+float Kd = 0.3;
+
+// ===== Filtrage =====
+float distance_filt = 15;
+const float alpha = 0.9; 
+
+// ===== Sonar =====
+float duration, distance;
+
+// ===== IMU =====
+const int MPU = 0x68;
+float AccX, AccY, AccZ;
+
+
+// ===== Madgwick =====
+Madgwick filter;
+
+// ===== Temps =====
+unsigned long lastTime = 0;
+float dt = 0.00; 
+
+// ===== Servo =====
+Servo myservo;
+float dist, cmd;
+float servo_cmd = 65;
+float servo_cmd_filt = 65;
+const int servoNeutral = 65;
+const int servoMin = 50;
+const int servoMax = 80;
+const float servoStep = 1.0;
+
+// =======================================================
+// ================= Lecture Sonar =======================
+// =======================================================
+void GetDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*0.0343)/2;
+}
+
+// =======================================================
+// =================== Lecture IMU =======================
+// =======================================================
+void lectureIMU(){
+  // ---------- LECTURE ACC ----------
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
+
+  // ---------- LECTURE GYRO ----------
+  /*
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+
+  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+  */
+}
+
+// =======================================================
+// ===================== Fuzzy ===========================
+// =======================================================
+void fuzzyGainTuning(float e, float de) {
+  float absE = abs(e);
+  //float absDE = abs(de);
+
+  if (absE > 10) {          // grosse erreur
+      Kp = 2.5;
+      Ki = 0.0;
+      Kd = 1.5;
+  }
+  else if (absE > 5) {      // erreur moyenne
+      Kp = 1.5;
+      Ki = 0.02;
+      Kd = 0.8;
+  }
+  else {                    // proche consigne
+      Kp = 0.6;
+      Ki = 0.05;
+      Kd = 0.3;
+  }
+}
+
+// =======================================================
+// ===================== Setup ===========================
+// =======================================================
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);                  
+  Wire.write(0x00);                  
+  Wire.endTransmission(true);
+  
+  myservo.attach(9);
+  myservo.write(servoNeutral);
+
+  filter.begin(100);
+  lastTime = millis();     
+  delay(20);
+}
+
+// =======================================================
+// ======================== LOOP =========================
+// =======================================================
+void loop() {
+  // ---------- TEMPS ----------
+  unsigned long now = millis();
+  dt = (now - lastTime) / 1000.0;
+  if (dt <= 0) return;
+  lastTime = now;
+
+  // ---------- LECTURE IMU -----------
+  lectureIMU();
+
+  // ---------- LECTURE SONAR ----------
+  GetDistance();
+  dist = constrain(distance, 0, 30);
+  
+  // ---------- FILTRAGE distance ----------
+  distance_filt = alpha * distance_filt + (1 - alpha) * dist;
+
+  // ---------- ERREUR ----------
+  error = distance_ref - distance_filt;
+
+  // ---------- DEADBAND ----------
+  if (abs(error) < 2) error = 0;
+
+  // ---------- INTEGRALE ----------
+  integral += error * dt;
+  integral = constrain(integral, -20, 20);
+
+  // ---------- DERIVEE ----------
+  float derivative = -(distance_filt - prev_target) / dt;
+  prev_target = distance_filt;
+  
+  // ---------- FUZZY ----------
+  fuzzyGainTuning(error, derivative);
+
+  // ---------- PID ----------
+  float output = Kp * error + Ki * integral + Kd * derivative; 
+  
+  // ---------- SERVO (limitation vitesse) ----------
+  servo_cmd_filt = servoNeutral - output;
+  //cmd = 0.8 * cmd + 0.2 * servo_cmd_filt;
+  cmd = servo_cmd_filt;
+  cmd = constrain(cmd, servoMin, servoMax);
+  myservo.write(cmd);
+
+  // ---------- DEBUG ----------
+  Serial.print("Distance: ");
+  Serial.print(distance_filt);
+  Serial.print(" | AccZ: ");
+  Serial.print(AccZ);
+  Serial.print(" | Servo: ");
+  Serial.print(cmd);
+  Serial.print(" | Kp: ");
+  Serial.print(Kp);
+  Serial.print(" | Kd: ");
+  Serial.print(Kd);
+  Serial.print(" | derive: ");
+  Serial.println(derivative);
+}
+
+// =======================================================
+// ====== MAIN (pour PlatformIO / Arduino Mega) ==========
+// =======================================================
+int main() {
+    init();       
+    setup();
+    while (true) {
+        loop();
+    }
+}
